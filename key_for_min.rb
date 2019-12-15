24b3f57bd779f7a6788bb1e38bcd1ef3d6897891
Marcel Rico <personal.mr4@gmail.com>
2019-12-15 12:50:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,12 @@
 def key_for_min_value(name_hash)
   return nil if name_hash == {}
   
+  past_value = nil
+  
   name_hash.each do |key,value|
     current_value = value
-    if(value)
+    if(past_value = nil)
+      cur
     
   end
 end
